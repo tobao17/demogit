100cc4b05d049894b797b865f10d41ab5d1cf820
phamtobao <phamtobao99@gmail.com>
2019-06-29 15:51:02 +0700

    add demo git2

--- CSS/26.gitdemo.js
+++ CSS/26.gitdemo.js
@@ -2,8 +2,11 @@ function mouse (name){
 this.name=name;
 }
 mouse.prototype.die=function(){
-    this.dead=true;
+    this.dead=false;
 }
+mouse.prototype.eat =function(){}
 modoule.exports=mouse;
+
 // workking directory : thu muc ma ban dang lam viec
 //staging area: nhung thay doi o workking directory se duoc add vao staging area
+//git repository:luu nhung thay dooi cua cac commitgit
