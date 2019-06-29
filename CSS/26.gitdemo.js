9eb361329ef85b3f596a10d0b8c4be2ee33ad5fa
phamtobao <phamtobao99@gmail.com>
2019-06-29 11:49:20 +0700

    add die method for gitdemo

--- CSS/26.gitdemo.js
+++ CSS/26.gitdemo.js
@@ -1,4 +1,9 @@
 function mouse (name){
 this.name=name;
 }
-module.exports=mouse;
+mouse.prototype.die=function(){
+    this.dead=true;
+}
+modoule.exports=mouse;
+// workking directory : thu muc ma ban dang lam viec
+//staging area: nhung thay doi o workking directory se duoc add vao staging area
