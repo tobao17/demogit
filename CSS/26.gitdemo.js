05b043537f796001f39cd43c9f5b233f2e98f3c7
phamtobao <phamtobao99@gmail.com>
2019-06-30 09:55:56 +0700

    aaaa

--- CSS/26.gitdemo.js
+++ CSS/26.gitdemo.js
@@ -1,5 +1,6 @@
 function mouse (name){
 this.name=name;
+<<<<<<< HEAD
 }
 mouse.prototype.die=function(){
     this.dead=false;
@@ -10,3 +11,6 @@ modoule.exports=mouse;
 // workking directory : thu muc ma ban dang lam viec
 //staging area: nhung thay doi o workking directory se duoc add vao staging area
 //git repository:luu nhung thay dooi cua cac commitgit
+=======
+}
+>>>>>>> parent of ea14dff... fix:missing exports
